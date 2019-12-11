8987ffe2a9cf91117ddafa937f5bafd3e41bce3d
xueht0525 <867997466@qq.com>
2019-12-11 22:02:24 +0800

    静态文件配置

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's-h4dq9*w(2ajrn@h%@!7m#c+qn(jz^cp&jz(8$v-^gte_a+s%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', ]
 
 # Application definition
 
