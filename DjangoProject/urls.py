1a94ba901d7c26e69bcae65d87ed66dbecbc6282
xueht0525 <867997466@qq.com>
2019-12-19 11:58:34 +0800

    init

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 
 from django.views import static
 from django.conf import settings
@@ -6,9 +6,7 @@ from django.conf.urls import url
 from . import view
 
 urlpatterns = [
-    url('login', view.login),
-    url('index', view.index),
-    url('welcome', view.welcome),
+    path('login/', view.hello),
     url(r'^static/(?P<path>.*)$', static.serve,
         {'document_root': settings.STATIC_ROOT}, name='static'),
 ]
--- DjangoProject/view.py
+++ DjangoProject/view.py
@@ -1,16 +1,5 @@
 from django.shortcuts import render
-from django.views.decorators.clickjacking import xframe_options_exempt
 
 
-def login(request):
-    return render(request, 'background/login.html')
-
-
-def index(request):
-    return render(request, 'background/index.html')
-
-
-# 防止 X-Frame-Options 挟持保护
-@xframe_options_exempt
-def welcome(request):
-    return render(request, 'background/welcome.html')
+def hello(request):
+    return render(request, 'login.html')
