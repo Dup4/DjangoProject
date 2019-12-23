d0701796868411e27830c783f40e32eb75979db7
xueht0525 <867997466@qq.com>
2019-12-23 09:44:08 +0800

    up

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  # 或者使用 mysql.connector.django
+        'NAME': 'website',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
